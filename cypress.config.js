6094c7b86e13b3e8b2d0b868463a45f5c995331b
Faris Andi Muhammad <farisandi458@gmail.com>
2026-03-14 16:09:07 +0700

    style: updated format eslint for ci/cd

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents (on, config) {
       // implement node event listeners here
     },
     video: false,
-    supportFile: false 
-  },
+    supportFile: false
+  }
 })
